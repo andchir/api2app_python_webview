8c18e143ad6ec8e1ba1c5651c9c7cb0832992b07
andrew <andrew@merlinsoftware.com>
2025-04-09 13:51:36 +0300

    check internet connection

--- api2app/src/api2app/app.py
+++ api2app/src/api2app/app.py
@@ -11,27 +11,19 @@ import requests
 SITE_URL = 'https://api2app.org'
 
 
-def check_internet_connection():
-    try:
-        response = requests.get('http://connectivitycheck.gstatic.com/generate_204', timeout=5)
-        return response.status_code == 204
-    except requests.exceptions.RequestException:
-        return False
-
-
 class api2app(toga.App):
     def startup(self):
         toga.Font.register('bootstrap-icons', 'resources/bootstrap-icons/fonts/bootstrap-icons.ttf')
 
-        if not check_internet_connection():
+        if not self.check_internet_connection():
             icon_label = toga.Label(
                 '\uf61c',
                 style=Pack(font_family='bootstrap-icons', font_size=60, padding=10, text_align='center')
             )
 
             text_label = toga.Label(
-                'Проверьте Ваше Интернет-соединение',
-                style=Pack(font_size=16, padding=10, text_align='center')
+                'Проверьте Интернет-соединение',
+                style=Pack(font_size=14, padding=10, text_align='center')
             )
             main_box = toga.Box(children=[icon_label, text_label], style=Pack(direction=COLUMN, alignment=CENTER, padding=20))
 
@@ -49,6 +41,13 @@ class api2app(toga.App):
         self.main_window = toga.MainWindow(title=self.formal_name, size=(1024, 768))
         self.main_window.content = main_box
         self.main_window.show()
+
+    def check_internet_connection(self):
+        try:
+            response = requests.get('http://connectivitycheck.gstatic.com/generate_204', timeout=5)
+            return response.status_code == 204
+        except requests.exceptions.RequestException:
+            return False
 
 
 def main():
